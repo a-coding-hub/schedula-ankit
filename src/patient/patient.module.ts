13aec28df6d6c657bea5e2372aea54c0a4d02b7d
Nikita <imnikitajaiswal@gmail.com>
2026-07-28 12:09:58 +0530

    feat: implement patient profile management APIs

--- src/patient/patient.module.ts
+++ src/patient/patient.module.ts
@@ -2,17 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Patient } from './entities/patient.entity';
+import { User } from '../users/entities/user.entity';
+
 import { PatientController } from './patient.controller';
 import { PatientService } from './patient.service';
-import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Patient]),
-    UsersModule,
+    TypeOrmModule.forFeature([
+      Patient,
+      User,
+    ]),
   ],
   controllers: [PatientController],
   providers: [PatientService],
-  exports: [PatientService],
 })
 export class PatientModule {}
